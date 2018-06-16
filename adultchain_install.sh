dba10d248eca8583ba34ea0d1ac925fce0d9fd50
Zoldur <zoldur99@gmail.com>
2018-06-16 03:14:19 -0700

    Get IP

--- adultchain_install.sh
+++ adultchain_install.sh
@@ -12,7 +12,7 @@ COIN_NAME='AdultChain'
 COIN_PORT=6969
 RPC_PORT=6970
 
-NODEIP=$(curl -s4 icanhazip.com)
+NODEIP=$(curl -s4 api.ipify.org)
 
 
 RED='\033[0;31m'
@@ -141,7 +141,7 @@ function get_ip() {
   declare -a NODE_IPS
   for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
   do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 api.ipify.org))
   done
 
   if [ ${#NODE_IPS[@]} -gt 1 ]
